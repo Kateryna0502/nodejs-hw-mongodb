2bfc165e7cc77a5672aab7a63904a329b9bb8c8a
Kateryna <mweoliss74@gmail.com>
2024-10-09 23:46:29 +0300

    a

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -13,8 +13,8 @@ async function initMongoConnection() {
     const url = process.env.MONGODB_URL;
     const db = process.env.MONGODB_DB;
 
-     await mongoose.connect(`mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority&appName=Node-hw`,
-    );
+     await mongoose.connect(`mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
+        );
      console.log("Mongo connection successfully established!");
  } catch (error) {
      console.error(error);
