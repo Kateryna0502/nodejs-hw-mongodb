c81a18c0c54404b9ba292fc628dcd1952485dfc5
Kateryna <mweoliss74@gmail.com>
2024-10-10 21:34:15 +0300

    ф

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { env } from '../utils/env.js';
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -8,13 +9,15 @@ dotenv.config();
 
 async function initMongoConnection() {
     try {
-    const user = process.env.MONGODB_USER;
-    const pwd = process.env.MONGODB_PASSWORD;
-    const url = process.env.MONGODB_URL;
-    const db = process.env.MONGODB_DB;
+    const user = env('MONGODB_USER');
+    const pwd = env('MONGODB_PASSWORD');
+    const url = env('MONGODB_URL');
+    const db = env('MONGODB_DB');
 
-     await mongoose.connect(`mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
+        await mongoose.connect(
+            `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
         );
+
      console.log("Mongo connection successfully established!");
  } catch (error) {
      console.error(error);
