a785a7ca1490c9c8d5ba9bf86f9e505651f1bf66
Kateryna <mweoliss74@gmail.com>
2024-10-12 13:21:32 +0300

    ф

--- src/server.js
+++ src/server.js
@@ -21,45 +21,50 @@ export const setupServer = () => {
 
 
     app.get('/contacts', async (req, res) => {
-        try {
+
             const contacts = await getAllContacts();
             res.status(200).json({
                 status: 200,
                 message: "Successfully found contacts!",
                 data: contacts,
             });
-        } catch (error) {
-    logger.error(error);
-    res.status(500).json({ message: 'Error retrieving contacts' });
-  }
+       });
+
+
+    app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+    if (!contact) {
+      res.status(404).json({ message: 'Contact not found' });
+      return;
+    }
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
     });
 
-
-    app.get('/contacts/:contactId', async (req, res) => {
-        const { contactId } = req.params;
-        try {
-            const contact = await getContactById(contactId); //
-            if (!contact) {
-                return res.status(404).json({ message: 'Contact not found' });
-            }
-            res.status(200).json({
-                status: 200,
-                message: `Successfully found contact with id ${contactId}!`,
-                data: contact,
-            });
-        } catch (error) {
-    logger.error(error);
-    res.status(500).json({ message: 'Error retrieving contact' });
-  }
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
     });
+  });
 
-
-
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-        pino.info(`Server is running on port ${PORT}`);
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
     });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    console.log(`Time: ${new Date().toLocaleString()}`);
+  });
 };
+
+
 
 
 
